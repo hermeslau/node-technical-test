a78040b01fe4a60bb62d054f98362a8b71fb313d
Hermes <hermeslau@hotmail.com>
2021-06-07 12:40:41 +1000

    Add error handling for incorrect path.

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ require('./routes/rss.route')(app);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 not found.');
+});
